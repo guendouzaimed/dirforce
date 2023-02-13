ddd98fe02fc2d57ef6b0bb2a723dfe522bec2792
guendouzaimed <111802013+guendouzaimed@users.noreply.github.com>
2023-02-13 20:26:56 +0000

    Update dirfinder.cpp

--- dirfinder.cpp
+++ dirfinder.cpp
@@ -2,22 +2,30 @@
 #include <curl/curl.h>
 #include <fstream>
 #include <string>
+#include "arguments.cpp"
 using namespace std;
 
 size_t noop_cb(void *ptr, size_t size, size_t nmemb, void *data) {
   return size * nmemb;
 }
-int main() {
-  CURL *curl;
-  CURLcode res;
-  string word;
-  ifstream wordlist("dirlist.txt");
-  string str = "http://www.google.com/";
-  while(getline(wordlist, word)) {
-    curl = curl_easy_init();
-    if(curl) {
-      str = "http://www.google.com/" + word;
+
+int main(int argc, char* argv[]) {
+  CURL *curl = curl_easy_init();
+  if(curl) {
+    string userurl;
+    int max_conn;
+    string wordlistfile;
+    arguments(argc, argv, &userurl, &max_conn, &wordlistfile);
+    CURLcode res;
+    string word;
+    ifstream wordlist(wordlistfile);
+    
+    
+    string str;
+    while(getline(wordlist, word)) {
+      str = userurl + word;
       curl_easy_setopt(curl, CURLOPT_URL, str.c_str());
+      curl_easy_setopt(curl, CURLOPT_MAXCONNECTS, max_conn);
       curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, noop_cb);
       curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 0);
       res = curl_easy_perform(curl);
@@ -33,14 +41,15 @@ int main() {
       else if (http_code < 400) {color = "\033[1;33m"; /* blue */}
       else if (http_code < 500) {color = "\033[1;35m"; /* purple */}
       else if (http_code < 600) {color = "\033[1;31m"; /* red */}
-      if (http_code != 404) {
+      if (http_code != 0 && http_code != 404) {
         cout << color << http_code << "   \033[0;37m/" << word;
         if (url) {
           cout << "   --> " <<"\033[0;34m" << url << endl;
         } else {cout << "\n";}
       }
-      curl_easy_cleanup(curl);
+      curl_easy_reset(curl);
     }
+    curl_easy_cleanup(curl);
   }
   return 0;
 }
