844bb87fdab0090ecdec54120de28154ee05b002
guendouzaimed <111802013+guendouzaimed@users.noreply.github.com>
2023-04-09 18:07:21 +0100

    Delete lineCounter.cpp

--- lineCounter.cpp
+++
@@ -1,9 +0,0 @@
-int lineCounter(std::string filename) {
-    std::ifstream infile(filename);
-    int line_count = 0;
-    std::string line;
-    while (std::getline(infile, line)) {
-        line_count++;
-    }
-    return line_count;
-}
