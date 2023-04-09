cd9619dd3958b2e4c9972da0985c1bff7e41c215
guendouzaimed <111802013+guendouzaimed@users.noreply.github.com>
2023-04-09 18:07:13 +0100

    Delete progress.cpp

--- progress.cpp
+++
@@ -1,8 +0,0 @@
-void progress() {
-    progressCounter++;
-    percent = "\r\033[1;37m" + to_string(((progressCounter * 100) / wordlistcount) / extension.size()) + "% " + to_string(progressCounter);
-    cout.flush();
-    cout << percent;
-    cout.flush();
-    cout << "\r";
-}
